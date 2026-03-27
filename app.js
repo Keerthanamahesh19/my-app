633c0555f6e8244028c66f30d526f37f6c17e410
Keerthanamahesh19 <keerthanamahesh.1616@gmail.com>
2026-03-27 11:16:14 +0530

    Fixed frontend display

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Serve static files
 app.use(express.static(__dirname));
+
+// Route for homepage
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
